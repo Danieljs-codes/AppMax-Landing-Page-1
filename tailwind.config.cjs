cb26e95d95c52192ae9e90c9e1b97a1a38c10c26
DanielDartDev <olamideonabiyi@student.oauife.edu.ng>
2023-02-25 18:46:01 +0100

    Built a couple of section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,8 @@ module.exports = {
       colors: {
         primary: '#377CFD',
         secondary: '#FD4C5C',
+        accent: '#000B33',
+        accent2: 'rgba(55, 124, 253, .1)',
       },
       fontFamily: {
         'HK-Grotesk': ['HK Grotesk', 'sans-serif'],
