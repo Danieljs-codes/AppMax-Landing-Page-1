5b2ceb40b50738d7a377cc8fb1d60ffc2cdebf2e
DanielDartDev <olamideonabiyi@student.oauife.edu.ng>
2023-02-25 15:42:55 +0100

    Initial Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#377CFD',
+        secondary: '#FD4C5C',
+      },
+      fontFamily: {
+        'HK-Grotesk': ['HK Grotesk', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
